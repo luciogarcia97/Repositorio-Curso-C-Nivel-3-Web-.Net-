46d6dd5d83cb65fd0e8ccface677905365d137cb
Lucio Agustin Garcia <116604189+luciogarcia97@users.noreply.github.com>
2024-03-10 00:05:01 -0300

    Inicio de home CG

--- PracticasCG/PracticasCG/Home.aspx
+++ PracticasCG/PracticasCG/Home.aspx
@@ -2,5 +2,5 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <h1>Aca apareceria todo</h1>
+    <h1>Aca apareceria todo del home</h1>
 </asp:Content>
--- PracticasCG/PracticasCG/principal.Master.designer.cs
+++ PracticasCG/PracticasCG/principal.Master.designer.cs
@@ -33,6 +33,24 @@ namespace PracticasCG
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// tbxBuscadorPrincipal control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbxBuscadorPrincipal;
+
+        /// <summary>
+        /// btnLogueo control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnLogueo;
+
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
